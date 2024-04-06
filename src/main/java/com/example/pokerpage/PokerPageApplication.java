3ff37089281a71da86293aaaffa20be96aed41f5
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-04-06 13:07:52 -0300

    🎉 init: First Commit

---
+++ src/main/java/com/example/pokerpage/PokerPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokerpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokerpage/PokerPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokerpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
